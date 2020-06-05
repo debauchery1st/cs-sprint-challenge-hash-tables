8b9d9b0a856c9121c3543ea9757a8b8ccbedd905
Trevor Martin <trevor.j.martin@gmail.com>
2020-06-05 13:47:19 -0400

    One-Way Flight Trip

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,32 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    route = []
+    start_at = list(
+        filter(lambda t: t[1].source == "NONE", enumerate(tickets)))[0]
+    end_at = list(
+        filter(lambda t: t[1].destination == "NONE", enumerate(tickets)))[0]
 
+    def filter_next(ticket):
+        return list(filter(lambda t: t[1].source == ticket.destination, enumerate(tickets)))[0]
+    airport = start_at
+    while airport != end_at:
+        if airport[1].source != "NONE":
+            route.append(airport[1].source)
+        airport = filter_next(airport[1])
+    route.append(airport[1].source)
+    route.append(end_at[1].destination)
     return route
+
+
+if __name__ == "__main__":
+    ticket_1 = Ticket("NONE", "PDX")
+    ticket_2 = Ticket("PDX", "DCA")
+    ticket_3 = Ticket("DCA", "NONE")
+
+    tickets = [ticket_1, ticket_2, ticket_3]
+
+    expected = ["PDX", "DCA", "NONE"]
+    result = reconstruct_trip(tickets, 3)
+    print(expected)
+    print(result)
